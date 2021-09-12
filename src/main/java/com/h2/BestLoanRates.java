cebba173f7fc1320178ce08653d3afc67aadbacc
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2021-09-12 14:32:37 +0100

    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -16,7 +16,7 @@ public class BestLoanRates {
         String name = scanner.nextLine();
         System.out.println("Hello "+ name);
 
-        System.out.println("Enter the loan terms (in years)");
+        System.out.println("Enter the loan term (in years)");
         int loanTermInYears = scanner.nextInt();
         float bestRate= getRates(loanTermInYears);
 
